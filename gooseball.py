b4fc97a19ea2919f15a59b6baa81bbaa958ddb0e
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2013-04-13 14:45:49 -0500

    Basic Game model

--- gooseball.py
+++ gooseball.py
@@ -1,5 +1,19 @@
 import webapp2
 
+from google.appengine.ext import db
+
+class Game(db.Model):
+    """Models an individual game"""
+    home_team = db.StringProperty()
+    away_team = db.StringProperty()
+    home_runs = db.IntegerProperty()
+    away_runs = db.IntegerProperty()
+    home_hits = db.IntegerProperty()
+    away_hits = db.IntegerProperty()
+    home_hrs = db.IntegerProperty()
+    away_hrs = db.IntegerProperty()
+    date = db.DateTimeProperty()
+
 class MainPage(webapp2.RequestHandler):
   def get(self):
       self.response.headers['Content-Type'] = 'text/plain'
