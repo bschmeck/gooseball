ed13b7be53f8fdb0ae5091dd12aa554e84a555a8
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2013-04-25 06:57:09 -0500

    Don't create a new Game object if we've already seen the game id before.

--- models/db_models.py
+++ models/db_models.py
@@ -13,20 +13,21 @@ class TeamStats(db.Model):
     hrs = db.IntegerProperty()
     hrs_allowed = db.IntegerProperty()
     win = db.IntegerProperty()
-    
-    @classmethod
-    def from_scoreboard_data(cls, data, home_or_away):
-        opponent = "away" if home_or_away == "home" else "home"
-        stats = TeamStats()
-        stats.team = data[home_or_away + "_name_abbrev"].upper()
-        stats.is_home_team = home_or_away == "home"
-        stats.runs = int(data[home_or_away + "_team_runs"])
-        stats.runs_allowed = int(data[opponent + "_team_runs"])
-        stats.hits = int(data[home_or_away + "_team_hits"])
-        stats.hits_allowed = int(data[opponent + "_team_hits"])
-        stats.hrs = int(data[home_or_away + "_team_hrs"])
-        stats.hrs_allowed = int(data[opponent + "_team_hrs"])
-        stats.win = 1 if stats.runs > stats.runs_allowed else 0
+
+    def __init__(self, home_or_away):
+        self.is_home_team = home_or_away == "home"
+        
+    def populate(self, data):
+        (team, opponent) = ("home", "away") if self.is_home_team else ("away", "home")
+
+        self.team = data[team + "_name_abbrev"].upper()
+        self.runs = int(data[team + "_team_runs"])
+        self.runs_allowed = int(data[opponent + "_team_runs"])
+        self.hits = int(data[team + "_team_hits"])
+        self.hits_allowed = int(data[opponent + "_team_hits"])
+        self.hrs = int(data[team + "_team_hrs"])
+        self.hrs_allowed = int(data[opponent + "_team_hrs"])
+        self.win = 1 if self.runs > self.runs_allowed else 0
         
 class Game(db.Model):
     """Models an individual game"""
@@ -37,13 +38,19 @@ class Game(db.Model):
 
     @classmethod
     def from_scoreboard_data(cls, data):
-        game = Game()
-        
         # id in the scoreboard looks like 2013/04/16/nynmlb-colmlb-1 but in URLs the gid
         # looks like 2013_04_16_nynmlb_colmlb_1, and we store the URL version of the gid
-        game.gid = re.sub("[/-]", "_", data["id"])
+        gid = re.sub("[/-]", "_", data["id"])
+
+        # If we've already seen this game, we're updating, otherwise we need to create a new game
+        game = Game.all().filter("gid = ", gid).get()
+        if not game:
+            game = Game()        
+            game.home_stats = TeamStats("home")
+            game.away_stats = TeamStats("away")
+
         game.date = datetime.strptime("%Y/%m/%d", data["original_date"])
 
-        game.home_stats = TeamStats.from_scoreboard_data(data, "home")
-        game.away_stats = TeamStats.from_scoreboard_data(data, "away")
+        game.home_stats.populate(data)
+        game.away_stats.populate(data)
         
