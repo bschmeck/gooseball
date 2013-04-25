e1a2f2e8c793742559c24ce6e2f0e65b98f2dad0
Ben Schmeckpeper <ben@aisle50.com>
2013-04-25 07:55:12 -0500

    Don't keep separate TeamStats in the db, just dump all the stats, home and away, in the Game.

--- models/db_models.py
+++ models/db_models.py
@@ -3,39 +3,21 @@ import re
 
 from google.appengine.ext import db
 
-class TeamStats(db.Model):
-    team = db.StringProperty()
-    is_home_team = db.BooleanProperty()
-    runs = db.IntegerProperty()
-    runs_allowed = db.IntegerProperty()
-    hits = db.IntegerProperty()
-    hits_allowed = db.IntegerProperty()
-    hrs = db.IntegerProperty()
-    hrs_allowed = db.IntegerProperty()
-    win = db.IntegerProperty()
-
-    def __init__(self, home_or_away):
-        self.is_home_team = home_or_away == "home"
-        
-    def populate(self, data):
-        (team, opponent) = ("home", "away") if self.is_home_team else ("away", "home")
-
-        self.team = data[team + "_name_abbrev"].upper()
-        self.runs = int(data[team + "_team_runs"])
-        self.runs_allowed = int(data[opponent + "_team_runs"])
-        self.hits = int(data[team + "_team_hits"])
-        self.hits_allowed = int(data[opponent + "_team_hits"])
-        self.hrs = int(data[team + "_team_hrs"])
-        self.hrs_allowed = int(data[opponent + "_team_hrs"])
-        self.win = 1 if self.runs > self.runs_allowed else 0
-        
 class Game(db.Model):
     """Models an individual game"""
     gid = db.StringProperty()
     date = db.DateTimeProperty()
-    home_stats = db.ReferenceProperty(TeamStats)
-    away_stats = db.ReferenceProperty(TeamStats)
-
+    home_team = db.StringProperty()
+    away_team = db.BooleanProperty()
+    home_runs = db.IntegerProperty()
+    away_runs = db.IntegerProperty()
+    home_hits = db.IntegerProperty()
+    away_hits = db.IntegerProperty()
+    home_hrs = db.IntegerProperty()
+    away_hrs = db.IntegerProperty()
+    home_win = db.IntegerProperty()
+    away_win = db.IntegerProperty()
+    
     @classmethod
     def from_scoreboard_data(cls, data):
         # id in the scoreboard looks like 2013/04/16/nynmlb-colmlb-1 but in URLs the gid
@@ -46,11 +28,17 @@ class Game(db.Model):
         game = Game.all().filter("gid = ", gid).get()
         if not game:
             game = Game()        
-            game.home_stats = TeamStats("home")
-            game.away_stats = TeamStats("away")
-
+            game.gid = gid
+            
         game.date = datetime.strptime("%Y/%m/%d", data["original_date"])
 
-        game.home_stats.populate(data)
-        game.away_stats.populate(data)
-        
+        game.home_team = data["home_name_abbrev"].upper()
+        game.away_team = data["home_name_abbrev"].upper()
+        game.home_runs = int(data["home_team_runs"])
+        game.away_runs = int(data["away_team_runs"])
+        game.home_hits = int(data["home_team_hits"])
+        game.away_hits = int(data["away_team_hits"])
+        game.home_hrs = int(data["home_team_hrs"])
+        game.away_hrs = int(data["away_team_hrs"])
+        game.home_win = 1 if game.home_runs > game.away_runs else 0
+        game.away_win = abs(1 - game.home_win)
